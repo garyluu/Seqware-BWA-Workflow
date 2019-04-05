32952c4f458792bb61ddd44f18eecc4d3d6fad3a
garyluu <garylau.work@gmail.com>
2019-04-05 13:23:39 -0400

    Test fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,9 @@ set -x
 gosu root chmod a+wrx /tmp
 WORK_DIR=$HOME
 cd $HOME
+
+# make all the imports read-able
+gosu root chmod a+r /var/lib/cwl/*
 gosu root chmod a+wrx $HOME
 gosu seqware bash -c "$*"
 #allow cwltool to pick up the results created by seqware
